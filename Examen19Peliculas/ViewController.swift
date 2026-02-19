58a1039c48f465353f096127b949011a15b681f1
wiski <wiski2311@gmail.com>
2026-02-19 16:24:12 +0100

    Initial Commit

---
+++ Examen19Peliculas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Examen19Peliculas
+//
+//  Created by Tardes on 19/2/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
